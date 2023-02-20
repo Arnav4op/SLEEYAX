f53a2f53a3cb7e080edc242add533104ebb91f47
Arnav4op <124982425+Arnav4op@users.noreply.github.com>
2023-02-20 15:53:39 +0530

    Add files via upload

--- Events/Reaction/messageReactionRemove.js
+++ Events/Reaction/messageReactionRemove.js
@@ -7,11 +7,11 @@ module.exports = async (client, reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
       
     if (rolePicker.hasOwnProperty(reaction.emoji.id || reaction.emoji)) {
-      const role = reaction.message.guild.roles.cache.get(rolePicker[reaction.emoji.id || reaction.emoji]);
       const member = reaction.message.guild.members.cache.get(user.id);
+      const role = member.roles.cache.get(rolePicker[reaction.emoji.id || reaction.emoji]);
       if (member && role && !user.bot) {
-        member.roles.add(role);
-        user.send(`Given you the role ${'`' + role.name + '`'}`);
+        member.roles.remove(role);
+        user.send(`Removed the role ${'`' + role.name + '`'}`);
       }
     }
   }
